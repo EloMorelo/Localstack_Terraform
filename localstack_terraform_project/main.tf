41629d000773314ddaa234bb0538cde370f15486
Jakub Ziętek <127032342+EloMorelo@users.noreply.github.com>
2024-05-27 03:15:30 +0200

    Update main.tf

--- localstack_terraform_project/main.tf
+++ localstack_terraform_project/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
 
-  access_key                  = "mock_access_key"
-  secret_key                  = "mock_secret_key"
+  access_key                  = "test"
+  secret_key                  = "test"
   region                      = "eu-west-2"
 
   s3_use_path_style           = true
